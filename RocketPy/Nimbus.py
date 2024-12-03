32146f89ec09a603895d1b7e6c5e53fa11e1be15
SafeeHussain <sh3723@ic.ac.uk>
2024-12-03 19:47:40 +0000

    Updated L9 rocketpy sims

---
+++ RocketPy/Nimbus.py
@@ -0,0 +1,130 @@
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir("..")
+
+# imports
+from rocketpy import Environment, Rocket, Flight, CompareFlights
+from Thanos import Thanos_R
+import datetime
+
+
+# Rocket
+# Nimbus includes payload and is used on the ascent
+# NimbusEmpty has no payload and is used on the descent
+# the individual payload + parafoil is not simulated, that's for our guided recovery sim
+Nimbus = Rocket(
+    radius=0.095,
+    mass=35.793,  # mass is excluding tanks and engine
+    inertia=(58.1, 58.1, 0.231),
+    power_off_drag="RocketPy/dragCurve.csv",
+    power_on_drag="RocketPy/dragCurve.csv",
+    center_of_mass_without_motor=4.28 - 2.3,
+    coordinate_system_orientation="tail_to_nose",
+)
+
+NimbusDescent = Rocket(
+    radius=0.095,
+    mass=49.892,  # mass is excluding tanks and engine
+    inertia=(42.2, 42.2, 0.222),
+    power_off_drag="RocketPy/dragCurve.csv",
+    power_on_drag="RocketPy/dragCurve.csv",
+    center_of_mass_without_motor=4.28 - 2.24,
+    coordinate_system_orientation="tail_to_nose",
+)
+
+Nimbus.add_motor(Thanos_R, position=0)
+
+nose_cone = Nimbus.add_nose(length=0.5, kind="ogive", position=4.08)
+nose_cone2 = NimbusDescent.add_nose(length=0.5, kind="ogive", position=4.08)
+
+
+fins = Nimbus.add_trapezoidal_fins(
+    n=3,
+    root_chord=0.36,
+    tip_chord=0.16,
+    sweep_length=0.24,
+    span=0.25,
+    position=0.36,
+    cant_angle=0,
+    radius=0.082,
+)
+
+fins2 = NimbusDescent.add_trapezoidal_fins(
+    n=3,
+    root_chord=0.36,
+    tip_chord=0.16,
+    sweep_length=0.24,
+    span=0.25,
+    position=0.36,
+    cant_angle=0,
+    radius=0.082,
+)
+
+
+
+boattail = Nimbus.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.302, position=0.302)
+boattail2 = NimbusDescent.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.302, position=0.302)
+
+# rail buttons?
+Nimbus.set_rail_buttons(
+    upper_button_position = 2.82,
+    lower_button_position = 0.36,
+    angular_position = 60,
+    )
+
+def drogue_trigger(p, h, y):
+    return True if y[5] < 0 else False
+
+def main_trigger(p, h, y):
+    return True if y[5] < 0 and h < 500 else False
+
+main = NimbusDescent.add_parachute(
+    name="main",
+    cd_s=29.128,
+    trigger=main_trigger,
+    sampling_rate=100,
+    lag=7,
+    noise = (0, 8.3, 0.5),
+)
+
+# add reefing to main parachute with a drogue
+drogue = NimbusDescent.add_parachute(
+    name="drogue",
+    cd_s=0.3936,
+    trigger=drogue_trigger,
+    sampling_rate=100,
+    lag=0,
+    noise = (0, 8.3, 0.5),
+)
+
+# we only want to run this if we are running this file specifically as a nominal sim
+
+if __name__ == "__main__":
+
+    # Environment
+    env = Environment(latitude=39.4751, longitude=-8.3764, elevation=0)
+    envtime = datetime.date.today() + datetime.timedelta(days = 1)
+    env.set_date((envtime.year, envtime.month, envtime.day, 12))  # UTC time
+    env.set_atmospheric_model(type="Forecast", file="GFS")
+
+    # draw rocket
+    Nimbus.draw()
+
+    # Flights
+    Ascent = Flight(rocket=Nimbus, environment=env, rail_length=12, inclination=86, heading=0, terminate_on_apogee=True, name="Ascent")
+    Descent = Flight(rocket=NimbusDescent, environment=env, rail_length=12, inclination=0, heading=0, initial_solution=Ascent, name="Descent")
+
+    # Results
+    comparison = CompareFlights([Ascent, Descent])
+    comparison.trajectories_3d(legend=True)
+
+    print("----- ENV INFO -----")
+    env.all_info()
+    print("----- THANOS INFO -----")
+    Thanos_R.all_info()
+    print("----- Nimbus INFO -----")
+    Nimbus.all_info()
+    print("----- ASCENT INFO -----")
+    Ascent.all_info()
+    print("----- DESCENT INFO -----")
+    Descent.info()
---
+++ RocketPy/Thanos.py
@@ -0,0 +1,80 @@
+# imports
+from math import exp
+from rocketpy import Fluid, LiquidMotor, CylindricalTank, MassFlowRateBasedTank
+
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir("..")
+
+# Define fluids
+ox_liq = Fluid(name="nitrous_l", density=1220)
+ox_gas = Fluid(name="nitrous_g", density=1.977)
+fuel_liq = Fluid(name="methanol_l", density=792.2)
+fuel_gas = Fluid(name="methanol_g", density=1.206)
+press_liq = Fluid(name="nitrogen_l", density=807)
+press_gas = Fluid(name="nitrogen_g", density=1.251)
+
+# Define tanks geometry
+# TODO: use custom endcaps if the geometry is not a perfect sphere
+ox_shape = CylindricalTank(radius = 0.19/2, height = 0.719, spherical_caps = False)
+fuel_shape = CylindricalTank(radius = 0.19/2, height = 0.2881, spherical_caps = False)
+press_shape = CylindricalTank(radius = 0.174/2, height = 0.56, spherical_caps = True)
+
+# Define tanks
+ox_tank = MassFlowRateBasedTank(
+    name="oxidizer tank",
+    geometry=ox_shape,
+    flux_time=9.35,
+    initial_liquid_mass=16.18,
+    initial_gas_mass=0,
+    liquid_mass_flow_rate_in=0,
+    liquid_mass_flow_rate_out=16.18 / 9.351,
+    gas_mass_flow_rate_in=0,
+    gas_mass_flow_rate_out=0,
+    liquid=ox_liq,
+    gas=ox_gas,
+)
+
+fuel_tank = MassFlowRateBasedTank(
+    name="fuel tank",
+    geometry=fuel_shape,
+    flux_time=9.35,
+    initial_liquid_mass=6.47,
+    initial_gas_mass=0,
+    liquid_mass_flow_rate_in=0,
+    liquid_mass_flow_rate_out=6.47/9.351,
+    gas_mass_flow_rate_in=0,
+    gas_mass_flow_rate_out=0,
+    liquid=fuel_liq,
+    gas=fuel_gas,
+)
+
+press_tank = MassFlowRateBasedTank(
+    name="nitrogen tank",
+    geometry=press_shape,
+    flux_time=9.35,
+    initial_liquid_mass=1.68,
+    initial_gas_mass=0,
+    liquid_mass_flow_rate_in=0,
+    liquid_mass_flow_rate_out=1.68 / 9.35,
+    gas_mass_flow_rate_in=0,
+    gas_mass_flow_rate_out=0,
+    liquid=press_liq,
+    gas=press_gas,
+)
+
+# Define motor
+# if the thrust curve is changed, define a specific impulse variable so we can calculate the mass flow rate of the propellants
+Thanos_R = LiquidMotor(
+    thrust_source="rocketpy/ThanosR40.eng",
+    dry_mass=3.1, # mass of engine, not tanks!
+    dry_inertia=(0.6050, 0.6094, 0.1004),
+    nozzle_radius=0.025,
+    center_of_dry_mass_position=1.0824,
+    nozzle_position=0,
+    burn_time=9.35,
+    coordinate_system_orientation="nozzle_to_combustion_chamber",
+)
+Thanos_R.add_tank(tank=ox_tank, position=4.31-2.92)
+Thanos_R.add_tank(tank=fuel_tank, position=4.31 - 2.38)
+Thanos_R.add_tank(tank=press_tank, position=4.31 - 1.59)
