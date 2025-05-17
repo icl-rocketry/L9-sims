f27f2f734766f0b2620fbd738db5a6f5e0d3df16
ben-groat <ben.groat22@imperial.ac.uk>
2025-05-17 12:29:13 +0100

    updated rocket script

--- RocketPy/Pluto.py
+++ RocketPy/Pluto.py
@@ -12,52 +12,52 @@ import datetime
 
 
 # Rocket
-# Pluto includes payload and is used on the ascent
-# PlutoEmpty has no payload and is used on the descent
+# Nimbus includes payload and is used on the ascent
+# NimbusEmpty has no payload and is used on the descent
 # the individual payload + parafoil is not simulated, that's for our guided recovery sim
-Pluto = Rocket(
-    radius=0.0925,
-    mass=53.866,  # mass is excluding tanks and engine
-    inertia=(66.5, 66.5, 0.242),
+Nimbus = Rocket(
+    radius=0.1,
+    mass=48.406,  # mass is excluding tanks and engine
+    inertia=(66.8, 66.8, 0.385),
     power_off_drag="RocketPy/dragCurve.csv",
     power_on_drag="RocketPy/dragCurve.csv",
-    center_of_mass_without_motor=4.34 - 2.35,
+    center_of_mass_without_motor= 4.31 - 2.48,
     coordinate_system_orientation="tail_to_nose",
 )
 
-PlutoDescent = Rocket(
-    radius=0.0925,
-    mass=53.866,  # mass is excluding tanks and engine
-    inertia=(66.5, 66.5, 0.242),
+NimbusDescent = Rocket(
+    radius=0.925,
+    mass=48.406,  # mass is excluding tanks and engine
+    inertia=(55.821, 55.821, 0.265),
     power_off_drag="RocketPy/dragCurve.csv",
     power_on_drag="RocketPy/dragCurve.csv",
-    center_of_mass_without_motor=4.34 - 2.35,
+    center_of_mass_without_motor=4.31 - 2.48,
     coordinate_system_orientation="tail_to_nose",
 )
 
-Pluto.add_motor(Thanos_R, position=0)
+Nimbus.add_motor(Thanos_R, position=0)
 
-nose_cone = Pluto.add_nose(length=0.7, kind="lvhaack", position=4.31)
-nose_cone2 = PlutoDescent.add_nose(length=0.7, kind="lvhaack", position=4.31)
+nose_cone = Nimbus.add_nose(length=0.7, kind="lvhaack", position=4.31)
+nose_cone2 = NimbusDescent.add_nose(length=0.7, kind="lvhaack", position=4.31)
 
 
-fins = Pluto.add_trapezoidal_fins(
+fins = Nimbus.add_trapezoidal_fins(
     n=3,
-    root_chord=0.41,
+    root_chord=0.4411,
     tip_chord=0.16,
     sweep_length=0.30,
-    span=0.22,
+    span=0.21,
     position=0.46,
     cant_angle=0,
     radius=0.1,
 )
 
-fins2 = PlutoDescent.add_trapezoidal_fins(
+fins2 = NimbusDescent.add_trapezoidal_fins(
     n=3,
-    root_chord=0.41,
+    root_chord=0.4411,
     tip_chord=0.16,
     sweep_length=0.30,
-    span=0.22,
+    span=0.21,
     position=0.46,
     cant_angle=0,
     radius=0.1,
@@ -65,11 +65,11 @@ fins2 = PlutoDescent.add_trapezoidal_fins(
 
 
 
-boattail = Pluto.add_tail(top_radius=0.0925, bottom_radius=0.06, length=0.293, position=0.293)
-boattail2 = PlutoDescent.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
+boattail = Nimbus.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
+boattail2 = NimbusDescent.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
 
 # rail buttons?
-Pluto.set_rail_buttons(
+Nimbus.set_rail_buttons(
     upper_button_position = 2.82,
     lower_button_position = 0.36,
     angular_position = 60,
@@ -79,11 +79,11 @@ def drogue_trigger(p, h, y):
     return True if y[5] < 0 else False
 
 def main_trigger(p, h, y):
-    return True if y[5] < 0 and h < 450 else False
+    return True if y[5] < 0 and h < 500 else False
 
-main = PlutoDescent.add_parachute(
+main = NimbusDescent.add_parachute(
     name="main",
-    cd_s=14.612,
+    cd_s=15.5509,
     trigger=main_trigger,
     sampling_rate=100,
     lag=7,
@@ -91,9 +91,9 @@ main = PlutoDescent.add_parachute(
 )
 
 # add reefing to main parachute with a drogue
-drogue = PlutoDescent.add_parachute(
+drogue = NimbusDescent.add_parachute(
     name="drogue",
-    cd_s=0.98033,
+    cd_s=0.3936,
     trigger=drogue_trigger,
     sampling_rate=100,
     lag=0,
@@ -111,11 +111,11 @@ if __name__ == "__main__":
     env.set_atmospheric_model(type="Forecast", file="GFS")
 
     # draw rocket
-    Pluto.draw()
+    Nimbus.draw()
 
     # Flights
-    Ascent = Flight(rocket=Pluto, environment=env, rail_length=12, inclination=86, heading=130, terminate_on_apogee=True, name="Ascent")
-    Descent = Flight(rocket=PlutoDescent, environment=env, rail_length=12, inclination=0, heading=130, initial_solution=Ascent, name="Descent")
+    Ascent = Flight(rocket=Nimbus, environment=env, rail_length=12, inclination=86, heading=130, terminate_on_apogee=True, name="Ascent")
+    Descent = Flight(rocket=NimbusDescent, environment=env, rail_length=12, inclination=0, heading=130, initial_solution=Ascent, name="Descent")
 
     # Results
     comparison = CompareFlights([Ascent, Descent])
@@ -125,8 +125,8 @@ if __name__ == "__main__":
     env.all_info()
     print("----- THANOS INFO -----")
     Thanos_R.all_info()
-    print("----- PLUTO INFO -----")
-    Pluto.all_info()
+    print("----- Nimbus INFO -----")
+    Nimbus.all_info()
     print("----- ASCENT INFO -----")
     Ascent.all_info()
     print("----- DESCENT INFO -----")
