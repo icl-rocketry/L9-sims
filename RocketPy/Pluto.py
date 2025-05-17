38b1ced2f7344c87671b7d6ba4ba444b7489ca38
ben-groat <ben.groat22@imperial.ac.uk>
2025-05-17 12:32:18 +0100

    renamed file

--- RocketPy/Pluto.py
+++ RocketPy/Pluto.py
@@ -12,52 +12,52 @@ import datetime
 
 
 # Rocket
-# Nimbus includes payload and is used on the ascent
-# NimbusEmpty has no payload and is used on the descent
+# Pluto includes payload and is used on the ascent
+# PlutoEmpty has no payload and is used on the descent
 # the individual payload + parafoil is not simulated, that's for our guided recovery sim
-Nimbus = Rocket(
-    radius=0.1,
-    mass=48.406,  # mass is excluding tanks and engine
-    inertia=(66.8, 66.8, 0.385),
+Pluto = Rocket(
+    radius=0.0925,
+    mass=53.866,  # mass is excluding tanks and engine
+    inertia=(66.5, 66.5, 0.242),
     power_off_drag="RocketPy/dragCurve.csv",
     power_on_drag="RocketPy/dragCurve.csv",
-    center_of_mass_without_motor= 4.31 - 2.48,
+    center_of_mass_without_motor=4.34 - 2.35,
     coordinate_system_orientation="tail_to_nose",
 )
 
-NimbusDescent = Rocket(
-    radius=0.925,
-    mass=48.406,  # mass is excluding tanks and engine
-    inertia=(55.821, 55.821, 0.265),
+PlutoDescent = Rocket(
+    radius=0.0925,
+    mass=53.866,  # mass is excluding tanks and engine
+    inertia=(66.5, 66.5, 0.242),
     power_off_drag="RocketPy/dragCurve.csv",
     power_on_drag="RocketPy/dragCurve.csv",
-    center_of_mass_without_motor=4.31 - 2.48,
+    center_of_mass_without_motor=4.34 - 2.35,
     coordinate_system_orientation="tail_to_nose",
 )
 
-Nimbus.add_motor(Thanos_R, position=0)
+Pluto.add_motor(Thanos_R, position=0)
 
-nose_cone = Nimbus.add_nose(length=0.7, kind="lvhaack", position=4.31)
-nose_cone2 = NimbusDescent.add_nose(length=0.7, kind="lvhaack", position=4.31)
+nose_cone = Pluto.add_nose(length=0.7, kind="lvhaack", position=4.31)
+nose_cone2 = PlutoDescent.add_nose(length=0.7, kind="lvhaack", position=4.31)
 
 
-fins = Nimbus.add_trapezoidal_fins(
+fins = Pluto.add_trapezoidal_fins(
     n=3,
-    root_chord=0.4411,
+    root_chord=0.41,
     tip_chord=0.16,
     sweep_length=0.30,
-    span=0.21,
+    span=0.22,
     position=0.46,
     cant_angle=0,
     radius=0.1,
 )
 
-fins2 = NimbusDescent.add_trapezoidal_fins(
+fins2 = PlutoDescent.add_trapezoidal_fins(
     n=3,
-    root_chord=0.4411,
+    root_chord=0.41,
     tip_chord=0.16,
     sweep_length=0.30,
-    span=0.21,
+    span=0.22,
     position=0.46,
     cant_angle=0,
     radius=0.1,
@@ -65,11 +65,11 @@ fins2 = NimbusDescent.add_trapezoidal_fins(
 
 
 
-boattail = Nimbus.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
-boattail2 = NimbusDescent.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
+boattail = Pluto.add_tail(top_radius=0.0925, bottom_radius=0.06, length=0.293, position=0.293)
+boattail2 = PlutoDescent.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
 
 # rail buttons?
-Nimbus.set_rail_buttons(
+Pluto.set_rail_buttons(
     upper_button_position = 2.82,
     lower_button_position = 0.36,
     angular_position = 60,
@@ -79,11 +79,11 @@ def drogue_trigger(p, h, y):
     return True if y[5] < 0 else False
 
 def main_trigger(p, h, y):
-    return True if y[5] < 0 and h < 500 else False
+    return True if y[5] < 0 and h < 450 else False
 
-main = NimbusDescent.add_parachute(
+main = PlutoDescent.add_parachute(
     name="main",
-    cd_s=15.5509,
+    cd_s=14.612,
     trigger=main_trigger,
     sampling_rate=100,
     lag=7,
@@ -91,9 +91,9 @@ main = NimbusDescent.add_parachute(
 )
 
 # add reefing to main parachute with a drogue
-drogue = NimbusDescent.add_parachute(
+drogue = PlutoDescent.add_parachute(
     name="drogue",
-    cd_s=0.3936,
+    cd_s=0.98033,
     trigger=drogue_trigger,
     sampling_rate=100,
     lag=0,
@@ -111,11 +111,11 @@ if __name__ == "__main__":
     env.set_atmospheric_model(type="Forecast", file="GFS")
 
     # draw rocket
-    Nimbus.draw()
+    Pluto.draw()
 
     # Flights
-    Ascent = Flight(rocket=Nimbus, environment=env, rail_length=12, inclination=86, heading=130, terminate_on_apogee=True, name="Ascent")
-    Descent = Flight(rocket=NimbusDescent, environment=env, rail_length=12, inclination=0, heading=130, initial_solution=Ascent, name="Descent")
+    Ascent = Flight(rocket=Pluto, environment=env, rail_length=12, inclination=86, heading=130, terminate_on_apogee=True, name="Ascent")
+    Descent = Flight(rocket=PlutoDescent, environment=env, rail_length=12, inclination=0, heading=130, initial_solution=Ascent, name="Descent")
 
     # Results
     comparison = CompareFlights([Ascent, Descent])
@@ -125,8 +125,8 @@ if __name__ == "__main__":
     env.all_info()
     print("----- THANOS INFO -----")
     Thanos_R.all_info()
-    print("----- Nimbus INFO -----")
-    Nimbus.all_info()
+    print("----- PLUTO INFO -----")
+    Pluto.all_info()
     print("----- ASCENT INFO -----")
     Ascent.all_info()
     print("----- DESCENT INFO -----")
--- RocketPy/UKMonteCarlo.py
+++ RocketPy/UKMonteCarlo.py
@@ -22,23 +22,22 @@ import datetime
 env = Environment(latitude=55, longitude=-2.64, elevation=325)
 envtime = datetime.date.today()
 env.set_date((envtime.year, envtime.month, envtime.day, 12))  # UTC time
-env.set_atmospheric_model(type="custom_atmosphere", wind_u=[[0, 3.4],[5000, 10.2]], wind_v=[[0, 1.3],[5000, 3.9]])
+# typical met values in july. Scale factor of 
+env.set_atmospheric_model(type="custom_atmosphere", wind_u=[[0, 3.4],[1000, 8.2],[5000, 10.9]], wind_v=[[0, 1.3],[1000, 3.4],[5000, 4.3]])
 
 # Creating the 'stochastic environment' counterpart
 stochastic_env = StochasticEnvironment(
     environment=env,
-    wind_velocity_x_factor=(1, 0.1),
-    wind_velocity_y_factor=(1, 0.1))
+    # add the effects of turbulence with the typical intensity factor. This adds random noise to the monte carlo sims
+    wind_velocity_x_factor=(1, 0.15),
+    wind_velocity_y_factor=(1, 0.15))
 # Reporting the attributes of the `StochasticEnvironment` object:
 stochastic_env.visualize_attributes()
 
 # Two rocket objects are created for the two different configurations during ascent & descent phases
-# NimbusAscent object includes the payload mass
-# NimbusDescent object has no payload
-# Note: The payload (and its deployment) is not simulated, there will be a separate guided recovery sim
 
 # Creating the (deterministic) rocket objects and flights ----------------------------------------------------
-NimbusAscent = Rocket(
+bipropAscent = Rocket(
     radius=0.1,
     mass=54.647,  # mass is excluding tanks and engine
     inertia=(69.032, 69.032, 0.417),
@@ -48,25 +47,13 @@ NimbusAscent = Rocket(
     coordinate_system_orientation="tail_to_nose",
 )
 
-# NimbusDescent = Rocket(
-#     radius=0.1,
-#     mass=54.647,  # mass is excluding tanks and engine
-#     inertia=(59.121, 59.121, 0.298),
-#     power_off_drag="RocketPy/dragCurve.csv",
-#     power_on_drag="RocketPy/dragCurve.csv",
-#     center_of_mass_without_motor=4.31 - 2.48,
-#     coordinate_system_orientation="tail_to_nose",
-# )
-
 # Adding motor only for ascent rocket
-NimbusAscent.add_motor(Thanos_R, position=0)
+bipropAscent.add_motor(Thanos_R, position=0)
 
-# Creating nose cone objects for ascent and descent phases
-nose_coneA = NimbusAscent.add_nose(length=0.35, kind="von karman", position=4.28)
-#nose_coneD = NimbusDescent.add_nose(length=0.35, kind="von karman", position=4.28)
+# Creating rocket components
+nose_coneA = bipropAscent.add_nose(length=0.35, kind="von karman", position=4.28)
 
-# Creating fins for ascent and descent rockets
-fins = NimbusAscent.add_trapezoidal_fins(
+fins = bipropAscent.add_trapezoidal_fins(
     n=3,
     root_chord=0.36,
     tip_chord=0.16,
@@ -77,89 +64,62 @@ fins = NimbusAscent.add_trapezoidal_fins(
     radius=0.082,
 )
 
-# fins2 = NimbusDescent.add_trapezoidal_fins(
-#     n=3,
-#     root_chord=0.36,
-#     tip_chord=0.16,
-#     sweep_length=0.24,
-#     span=0.25,
-#     position=0.36,
-#     cant_angle=0,
-#     radius=0.082,
-# )
+boattail = bipropAscent.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
 
-# Creating boattails for ascent and descent rockets
-boattail = NimbusAscent.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
-#boattail2 = NimbusDescent.add_tail(top_radius=0.1, bottom_radius=0.0821, length=0.41, position=0.42)
-
-# Setting rail buttons only for ascent rocket
-NimbusAscent.set_rail_buttons(
+bipropAscent.set_rail_buttons(
     upper_button_position = 2.82,
     lower_button_position = 0.36,
     angular_position = 60,
     )
 
+# parachute triggers - change the main trigger to 500 for nominal flight, 5000 (or anything above apogee) for main at apogee case
 def drogue_trigger(p, h, y):
     return True if y[5] < 0 else False
 
 def main_trigger(p, h, y):
-    return True if y[5] < 0 and h < 5000 else False
+    return True if y[5] < 0 and h < 500 else False
 
-main = NimbusAscent.add_parachute(
+# add the chutes to the rocket
+main = bipropAscent.add_parachute(
     name="main",
-    cd_s=10.4,
+    cd_s=9.8,
     trigger=main_trigger,
     sampling_rate=100,
-    lag=7,
-    noise = (0, 0, 0), # Set to 0, noise simulates pressure sensor which is not implemented in this sim
+    lag=3,
+    noise = (0, 0, 0),
 )
 
-# add reefing to main parachute with a drogue
-drogue = NimbusAscent.add_parachute(
+drogue = bipropAscent.add_parachute(
     name="drogue",
     cd_s=0.3936,
     trigger=drogue_trigger,
     sampling_rate=100,
-    lag=0,
+    lag=1,
     noise = (0, 0, 0), # Set to 0, noise simulates pressure sensor which is not implemented in this sim
 )
 
 
-# # draw rocket
-# NimbusAscent.draw()
 
 # Flights
-Ascent = Flight(rocket=NimbusAscent, environment=env, rail_length=12, inclination=90, heading=0, name="Ascent")
-#Descent = Flight(rocket=NimbusDescent, environment=env, rail_length=12, inclination=0, heading=0, initial_solution=Ascent, name="Descent")
+Ascent = Flight(rocket=bipropAscent, environment=env, rail_length=9, inclination=85, heading=60, name="Ascent")
 
 # Results
 comparison = CompareFlights([Ascent, Ascent])
 comparison.trajectories_3d(legend=True)
 
-# print("----- ASCENT INFO -----")
-# Ascent.info()
-# print("----- DESCENT INFO -----")
-# Descent.info()
+
+
 
 # MONTE CARLO SECTION OF SCRIPT ---------------------------------------------------------
 # Note: The uncertainties assigned to the components in this script are arbitrary - will finalise later
 
-# Creating the corresponding 'stochastic rocket' objects -------------------
-stochastic_Ascent = StochasticRocket(
-    rocket=NimbusAscent,
+# Creating the corresponding 'stochastic rocket' objects
+stochasticBiprop = StochasticRocket(
+    rocket=bipropAscent,
     mass=(54.647, 1, "normal"),
 )
-# Reporting the attributes of the `StochasticRocket` object:
-# stochastic_Ascent.visualize_attributes()
 
-# stochastic_Descent = StochasticRocket(
-#     rocket=NimbusDescent,
-#     mass=(54.647, 1, "normal"),
-# )
-# Reporting the attributes of the `StochasticRocket` object:
-# stochastic_Ascent.visualize_attributes()
-
-# Creating the 'stochastic aerosurface' objects for ascent
+# Creating the stochastic objects for ascent
 stochastic_nose_coneA = StochasticNoseCone(
     nosecone=nose_coneA,
 )
@@ -172,21 +132,7 @@ stochastic_tailA = StochasticTail(
     tail=boattail,
 )
 
-# # Creating the 'stochastic aerosurface' objects for descent
-# stochastic_nose_coneD = StochasticNoseCone(
-#     nosecone=nose_coneD,
-# )
-
-# stochastic_fin_setD = StochasticTrapezoidalFins(
-#     trapezoidal_fins=fins2,
-# )
-
-# stochastic_tailD = StochasticTail(
-#     tail=boattail2,
-# )
-
 # Converting Liquid Motor object to a GenericMotor to be compatible with 'Stochastic' objects
-# Note: From the docs, apparently this object is less accurate than Liquid/SolidMotor (verify values)
 GenericThanos_R = GenericMotor(
     thrust_source="OpenRocket/ThanosR40.eng",
     burn_time=6.53,
@@ -204,54 +150,23 @@ GenericThanos_R = GenericMotor(
     coordinate_system_orientation="nozzle_to_combustion_chamber",
     )
 
-# Adding components to the 'stochastic rocket' objects
-# Note: Bug in code (not sure if it's me or Rocketpy) doesn't allow multiple sets of fins to be added to stochastic rocket
-#       The omission of canards for the ascent phase should lead to the landing ellipses being smaller than reality,
-#       this is due to the destabilising effects of the canards in pitch & yaw not being included.
-#       The omission of the canards for the descent phase should have negligible impact on the results,
-#       this is due to the canards only developing small aerodynamic forces at low speeds (using parachutes)
 
 # Ascent
-stochastic_Ascent.add_motor(GenericThanos_R, position=0.001)
-stochastic_Ascent.add_nose(stochastic_nose_coneA, position=(4.28, 0.001))
-stochastic_Ascent.add_trapezoidal_fins(stochastic_fin_setA, position=0.32)
-# stochastic_Ascent.add_trapezoidal_fins(stochastic_canardsA, position=3.04)
-stochastic_Ascent.add_tail(stochastic_tailA)
-
-# # Descent
-# stochastic_Descent.add_nose(stochastic_nose_coneA, position=(4.28, 0.001))
-# stochastic_Descent.add_trapezoidal_fins(stochastic_fin_setD, position=0.32)
-# # stochastic_Descent.add_trapezoidal_fins(stochastic_canardsD, position=3.04)
-# stochastic_Descent.add_tail(stochastic_tailD)
-stochastic_Ascent.add_parachute(main)
-stochastic_Ascent.add_parachute(drogue)
+stochasticBiprop.add_motor(GenericThanos_R, position=0.001)
+stochasticBiprop.add_nose(stochastic_nose_coneA, position=(4.28, 0.001))
+stochasticBiprop.add_trapezoidal_fins(stochastic_fin_setA, position=0.32)
+stochasticBiprop.add_tail(stochastic_tailA)
+stochasticBiprop.add_parachute(main)
+stochasticBiprop.add_parachute(drogue)
 
 # Monte Carlo Flights -----------------------------------------------------------
 
 # Ascent flight
 stochastic_flightAscent = StochasticFlight(
     flight=Ascent,
-    inclination=(90, 1),  # mean = 86, std = 1
-    heading=(45, 15),  # mean = 0, std = 2
+    inclination=(85, 0.25),  # mean = 87, std. dev = 0.25 - taken from accuracies in launch rail alignment
+    heading=(60, 1),  # mean = 60, std = 1 - taken from accuracies in launch rail alignment. Launch with the wind for safety
 )
-
-# # Extract the final state at the last timestep from the Ascent flight object
-# t_final = Ascent.apogee_time
-# final_state_vectorA = Ascent.get_solution_at_time(t_final)
-
-# # Print the final ascent state vector for verification
-# print("Final State Vector (Initial Solution):", final_state_vectorA)
-
-# # Defining another descent flight since 'StochasticFlight' object has to be initialised with a tuple/list and not a 'Flight' object as before
-# Descent2 = Flight(rocket=NimbusDescent, environment=env, rail_length=12, inclination=0, heading=0, initial_solution=final_state_vectorA, name="Descent2")
-
-# # Descent flight
-# stochastic_flightDescent = StochasticFlight(
-#     flight=Descent2,                       # Pass the existing flight object
-#     inclination=0,                         # Define inclination or randomize it
-#     heading=0,                             # Define heading or randomize it
-#     initial_solution=final_state_vectorA,  # Use the extracted final ascent state vector
-# )
 
 # Initialising Monte Carlo objects for the sims
 numberOfSims = 100 # Setting the number of Monte Carlo sims to run
@@ -260,28 +175,13 @@ numberOfSims = 100 # Setting the number of Monte Carlo sims to run
 test_dispersionAscent = MonteCarlo(
     filename="ascent",
     environment=stochastic_env,
-    rocket=stochastic_Ascent,
+    rocket=stochasticBiprop,
     flight=stochastic_flightAscent,
 )
 
 # Running the Monte Carlo simulations for the ascent phase
-# Note: The result of this call should be multiple ascent flights with ballistic descents and no payload deployment
 test_dispersionAscent.simulate(number_of_simulations=numberOfSims, append=False)
-
-
-# # Data for descent phase
-# test_dispersionDescent = MonteCarlo(
-#     filename="descent",
-#     environment=stochastic_env,
-#     rocket=stochastic_Descent,
-#     flight=stochastic_flightDescent,
-# )
-
-# # Running the Monte Carlo simulations for the descent phase
-# # Note: The result of this call should be multiple descent flights, all initialised using the 'nominal' ascent flight (and payload has been deployed)
-# test_dispersionDescent.simulate(number_of_simulations=numberOfSims, append=False)
 
 # Plotting the simulated apogee and landing zones
 test_dispersionAscent.plots.ellipses()
-# test_dispersionDescent.plots.ellipses()
 test_dispersionAscent.prints.all()
