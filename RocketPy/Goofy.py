f54d08c4f3108d192be729187dda90c76d8b9109
daniel-dyon <daniel.dyon@gmail.com>
2025-06-22 12:16:30 +0100

    big changes for prop
    also fixed a few goofed lengths, and then goofed some masses

--- RocketPy/Goofy.py
+++ RocketPy/Goofy.py
@@ -12,23 +12,23 @@ ox_gas = Fluid(name="nitrous_g", density=1.977)
 fuel_liq = Fluid(name="ethanol_l", density=789)
 fuel_gas = Fluid(name="ethanol_g", density=0.79)
 press_liq = Fluid(name="nitrogen_l", density=807)
-press_gas = Fluid(name="nitrogen_g", density=1.251)
+press_gas = Fluid(name="nitrogen_g", density=12.51*17)
 
 # Define tanks geometry
 # TODO: use custom endcaps if the geometry is not a perfect sphere
-ox_shape = CylindricalTank(radius = 0.19/2, height = 0.719, spherical_caps = False)
-fuel_shape = CylindricalTank(radius = 0.19/2, height = 0.2881, spherical_caps = False)
-press_shape = CylindricalTank(radius = 0.174/2, height = 0.56, spherical_caps = True)
+ox_shape = CylindricalTank(radius = 0.185/2, height = 0.71, spherical_caps = False)
+fuel_shape = CylindricalTank(radius = 0.185/2, height = 0.2881, spherical_caps = False)
+press_shape = CylindricalTank(radius = 0.157/2, height = 0.56, spherical_caps = True)
 
 # Define tanks
 ox_tank = MassFlowRateBasedTank(
     name="oxidizer tank",
     geometry=ox_shape,
-    flux_time=9.35,
-    initial_liquid_mass=16.18,
+    flux_time=7.95,
+    initial_liquid_mass=16.06,
     initial_gas_mass=0,
     liquid_mass_flow_rate_in=0,
-    liquid_mass_flow_rate_out=16.18 / 9.36,
+    liquid_mass_flow_rate_out=16.06 / 7.96,
     gas_mass_flow_rate_in=0,
     gas_mass_flow_rate_out=0,
     liquid=ox_liq,
@@ -38,11 +38,19 @@ ox_tank = MassFlowRateBasedTank(
 fuel_tank = MassFlowRateBasedTank(
     name="fuel tank",
     geometry=fuel_shape,
+<<<<<<< Updated upstream
     flux_time=9.35,
     initial_liquid_mass=6.44,
     initial_gas_mass=0,
     liquid_mass_flow_rate_in=0,
     liquid_mass_flow_rate_out=6.44 / 9.36,
+=======
+    flux_time=7.95,
+    initial_liquid_mass=5.35,
+    initial_gas_mass=0,
+    liquid_mass_flow_rate_in=0,
+    liquid_mass_flow_rate_out= 5.35 / 7.96,
+>>>>>>> Stashed changes
     gas_mass_flow_rate_in=0,
     gas_mass_flow_rate_out=0,
     liquid=fuel_liq,
@@ -52,13 +60,13 @@ fuel_tank = MassFlowRateBasedTank(
 press_tank = MassFlowRateBasedTank(
     name="nitrogen tank",
     geometry=press_shape,
-    flux_time=9.35,
-    initial_liquid_mass=1.68,
-    initial_gas_mass=0,
+    flux_time=7.95,
+    initial_liquid_mass=0,
+    initial_gas_mass=2.06,
     liquid_mass_flow_rate_in=0,
-    liquid_mass_flow_rate_out=1.68 / 9.36,
+    liquid_mass_flow_rate_out=0,
     gas_mass_flow_rate_in=0,
-    gas_mass_flow_rate_out=0,
+    gas_mass_flow_rate_out= 2.06 / 7.95,
     liquid=press_liq,
     gas=press_gas,
 )
@@ -67,12 +75,12 @@ press_tank = MassFlowRateBasedTank(
 # if the thrust curve is changed, define a specific impulse variable so we can calculate the mass flow rate of the propellants
 Goofy = LiquidMotor(
     thrust_source="rocketpy/ThanosR40.eng",
-    dry_mass=3.1, # mass of engine, not tanks!
+    dry_mass=2.7, # mass of engine, not tanks!
     dry_inertia=(0.6050, 0.6094, 0.1004),
     nozzle_radius=0.025,
-    center_of_dry_mass_position=1.0824,
+    center_of_dry_mass_position=0.28,
     nozzle_position=0,
-    burn_time=9.94,
+    burn_time=7.95,
     coordinate_system_orientation="nozzle_to_combustion_chamber",
 )
 Goofy.add_tank(tank=ox_tank, position=4.31 - 2.92 - 0.719/2)
